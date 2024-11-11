d1f5b2a3258a5873825caff2bfdcd3825d0bda57
ITS-Admin <ITS-Admin@PENZIAS-TR-46>
2024-11-11 15:49:06 +0100

    Projektdateien hinzufügen.

---
+++ GeneralSnippets/RValueLValue/RValueLValue.cpp
@@ -0,0 +1,89 @@
+// =====================================================================================
+// LValue / RValue
+// =====================================================================================
+
+module modern_cpp:rvalue_lvalue;
+
+namespace LValueRValue {
+
+    // lvalue reference
+    static void sayHello(std::string& message) {
+        std::println("sayHello [std::string&]:  {}", message);
+    }
+
+    // rvalue reference
+    static void sayHello(std::string&& message) {
+        std::println("sayHello [std::string&&]: {}", message);
+    }
+#
+
+    static void test01() {
+
+        std::string a = "Hello";
+        std::string b = " World";
+
+        sayHello(a);
+        sayHello(a + b); // +:  Aneinanderhängen
+    }
+
+    // -------------------------------------------------------------------
+
+    static void helper(std::string&& message) {
+
+        sayHello(message);  // sayHello (&):  message ist ein Name (& Ref)
+                            // sayHello (&&): message ist && Ref.
+    
+        sayHello( std::move( message ) );
+    }
+
+    static void test02() {
+
+        //std::string a = "Hello";
+        //helper(a);
+
+        helper(std::string("Where are we going ..."));
+    }
+
+    // -------------------------------------------------------------------
+
+    static void test03() {
+
+        std::string s = "Hello";
+
+        sayHello(s);
+        // versus
+        sayHello(std::move(s));  // casts an lvalue to an rvalue
+    }
+
+    // -------------------------------------------------------------------
+
+    static void test04() {
+
+        // int& i = 2;          // invalid: reference to a constant
+
+        // int a = 2;
+        // int b = 3;
+
+        // int& k = a + b;     // invalid: reference to a temporary
+
+        int&& i = 2;           // works: reference to a constant
+
+        int a = 2;
+        int b = 3;
+
+        int&& k = a + b;       // works: reference to a temporary
+    }
+}
+
+void main_rvalue_lvalue()
+{
+    using namespace LValueRValue;
+    test01();
+    test02();
+    test03();
+    test04();
+}
+
+// =====================================================================================
+// End-of-File
+// =====================================================================================
